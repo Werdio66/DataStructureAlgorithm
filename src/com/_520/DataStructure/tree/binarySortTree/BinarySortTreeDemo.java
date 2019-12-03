fce0613e1345830138175d6230387eb65f05403c
Werdio66 <1171472801@qq.com>
2019-12-03 20:03:38 +0800

    二叉排序树结点的删除

--- src/com/_520/DataStructure/tree/binarySortTree/BinarySortTreeDemo.java
+++ src/com/_520/DataStructure/tree/binarySortTree/BinarySortTreeDemo.java
@@ -2,13 +2,16 @@ package com._520.DataStructure.tree.binarySortTree;
 
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 0};
         BinarySortTree binarySortTree = new BinarySortTree();
         for (int i = 0; i < arr.length; i++) {
             binarySortTree.add(new Node(arr[i]));
         }
+        System.out.println("删除前");
         binarySortTree.midOrder();
-
+        binarySortTree.deleteNode(7);
+        System.out.println("删除后");
+        binarySortTree.midOrder();
     }
 
     static class BinarySortTree{
@@ -27,6 +30,76 @@ public class BinarySortTreeDemo {
             }
             return root.serachParent(value);
         }
+
+        /**
+         *  查找右结点中的值最小的子结点
+         * @param node      要查找的结点
+         * @return          最小值
+         */
+        private int findMinValueInRightNode(Node node){
+            Node target = node;
+            // 查找最小的结点
+            while (target.left != null){
+                target = target.left;
+            }
+            // 拿到最小的值
+            int value = target.value;
+            // 删除指定值的结点
+            deleteNode(value);
+            //
+            return value;
+        }
+        // 删除指定值的结点
+        public void deleteNode(int value){
+            // 拿到要删除的结点
+            Node targetNode = serach(value);
+            if (targetNode == null){
+                System.out.println("没有找到要删除的结点");
+                return;
+            }
+            // 如果当前结点是根结点，且这颗树中只有这一个结点
+            if (root.left == null && root.right == null){
+                // 删除这个结点
+                root = null;
+                return;
+            }
+            // 拿到删除结点的父结点
+            Node parentNode = serachparent(value);
+            // 说明要删除的结点是叶子结点
+            if (targetNode.left == null && targetNode.right == null) {
+                // 说明要删除的结点是父结点的左结点
+                if (parentNode.left.value == targetNode.value) {
+                    // 删除结点
+                    parentNode.left = null;
+                }
+                // 说明要删除的结点是右结点
+                if (parentNode.right.value == targetNode.value) {
+                    // 删除结点
+                    parentNode.right = null;
+                }
+            }else if (targetNode.left != null && targetNode.right != null){ // 要删除的结点有俩个子结点
+                // 找到删除结点右子树中的最小值赋给当前要删除的结点
+                targetNode.value = findMinValueInRightNode(targetNode.right);
+
+            }else { // 删除的结点有一个子结点
+                if (targetNode.left != null) {   // 要删除的结点有一个左子结点
+                    // 要删除的结点是它的父结点的左结点
+                    if (parentNode.left.value == targetNode.value) {
+                        parentNode.left = targetNode.left;
+                    }else { // 要删除结点的是它的父结点的右子结点
+                        parentNode.right = targetNode.left;
+                    }
+                }else {     // 删除的结点有一个右子结点
+                    // 要删除的结点是它的父结点的左结点
+                    if (parentNode.left.value == targetNode.value) {
+                        parentNode.left = targetNode.right;
+                    }else { // 要删除结点的是它的父结点的右子结点
+                        parentNode.right = targetNode.right;
+                    }
+                }
+            }
+        }
+
         // 增加结点
         public void add(Node node){
             if (root == null){
@@ -95,7 +168,7 @@ public class BinarySortTreeDemo {
             if (this.value == value) {
                 return this;
             }
-            if (this.value < value) {
+            if (value < this.value) {
                 if (this.left != null) {
                     return this.left.serach(value);
                 }
