6b991466f4b1970e62c2b30aba334d18eca1f710
Werdio66 <1171472801@qq.com>
2019-12-04 13:16:22 +0800

    FCFS和SJF

--- src/com/_520/Algorithm/operating_system/JobScheduling.java
+++ src/com/_520/Algorithm/operating_system/JobScheduling.java
@@ -9,8 +9,7 @@ public class JobScheduling {
     private static List<PCB> enterPcb = new LinkedList<>();
     // 存放已经处理过的pcb
     private static List<PCB> dealPcb = new ArrayList<>();
-    // 当前执行的进程
-    private static PCB nowPrecess = null;
+
 
     /**
      *  初始化
@@ -27,7 +26,6 @@ public class JobScheduling {
         allPcb.add(pcb4);
         allPcb.add(pcb5);
         allPcb.sort(new ArrivedSort());
-        System.out.println(allPcb);
     }
     /**
      *  先来先服务
@@ -35,7 +33,7 @@ public class JobScheduling {
     private static void FCFS() {
         PcbQueue queue = new PcbQueue();
         queue.enQueue();
-        while (!enterPcb.isEmpty()){
+        while (!isFinish()){
             queue.deQueue();
             queue.enQueue();
         }
@@ -44,31 +42,45 @@ public class JobScheduling {
 
     /**
      *  短作业
-     * @param pcb   作业
      */
-    private static void SJF(List<PCB> pcb){
-
+    private static void SJF(){
+        PcbQueue queue = new PcbQueue();
+        allPcb.sort(new ServiceSort());
+        queue.enQueue();
+        while (!isFinish()){
+            queue.deQueue();
+            queue.enQueue();
+        }
+        queue.print();
     }
 
     public static void main(String[] args) {
        init();
-       FCFS();
+//       FCFS();
+        SJF();
     }
 
+    private static boolean isFinish(){
+        return dealPcb.size() == allPcb.size();
+    }
 
     static class PcbQueue{
         // 记录当前遍历的是哪个pcb
         private int count = 0;
+        // 记录当前时间
         private int nowTime = 0;
-        private int waitTime = 0;
+        // 当前执行的进程
+        private PCB nowPrecess = null;
         // 入队，
         public void enQueue(){
             while (count < allPcb.size() && enterPcb.size() == 0){
-               if (allPcb.get(count).arrivedTime > nowTime)
-                   waitTime = allPcb.get(count).arrivedTime - nowTime;
+
+                while (allPcb.get(count).arrivedTime > nowTime){
+                    nowTime++;
+                }
 
                 // 将符合数据的额所有pcb加入enterPcb中
-                if ((allPcb.get(count).arrivedTime + waitTime) <= nowTime){
+                if (allPcb.get(count).arrivedTime <= nowTime){
                     enterPcb.add(allPcb.get(count));
                     count++;
                 }else {
@@ -79,9 +91,12 @@ public class JobScheduling {
         // 出队，为当前进程设置相关的字段值
         public void deQueue(){
             // 当前进程为第一个进入队列的
+            if (enterPcb.size() == 0){
+                return;
+            }
             nowPrecess = enterPcb.remove(0);
             // 计算开始时间，即为上一个进程的结束时间
-            nowPrecess.startTime = nowTime + waitTime;
+            nowPrecess.startTime = nowTime;
             // 计算结束时间，该进程开始时间+服务时间
             nowPrecess.finshTime = nowPrecess.startTime + nowPrecess.doTime;
             // 计算周转时间
@@ -90,19 +105,16 @@ public class JobScheduling {
             nowPrecess.aveRoundTime = (double) nowPrecess.roundTime / nowPrecess.doTime;
             // 获得结束时间，即当前时间，方便判断剩下的进程是否已到达
             nowTime = nowPrecess.finshTime;
-            // 计算等待时间
-            nowPrecess.waitTime = waitTime;
             //经处理过数据后加入dealPcb容器
             dealPcb.add(nowPrecess);
-            waitTime = 0;
         }
         public void print(){
-            System.out.println("进程名 到达时间  服务时间   开始时间  完成时间  周转时间  带权周转时间  等待时间");
+            System.out.println("进程名 到达时间  服务时间   开始时间  完成时间  周转时间  带权周转时间");
             for (PCB pcb:dealPcb
                  ) {
                 System.out.println(pcb.name + "        " + pcb.arrivedTime + "         " + pcb.doTime + "        " +
                         pcb.startTime + "        " + pcb.finshTime + "         " +
-                        pcb.roundTime + "          " + pcb.aveRoundTime + "          " + pcb.waitTime);
+                        pcb.roundTime + "          " + pcb.aveRoundTime);
             }
         }
     }
@@ -122,6 +134,7 @@ class ServiceSort implements Comparator<PCB>{
 
     @Override
     public int compare(PCB o1, PCB o2) {
-        return 0;
+
+        return o1.doTime - o2.doTime;
     }
 }
